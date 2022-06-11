58551329451aa06004518c8e53b0e82559f20d59
Novotnde <novotnde@hotmail.com>
2022-06-11 16:04:20 +0200

    initial comit

--- students/task02/task.js
+++ students/task02/task.js
@@ -9,6 +9,70 @@
  * @param {object} dtoIn input data
  * @return {object} output data
 **/
+
 function main(dtoIn={}) {
+
+    validate(dtoIn);
+    return getAverageClassData(dtoIn);
+}
+
+function getAverageClassData(dtoIn){
+
+    let minBirthDate = new Date();
+    let maxBirthDate = new Date();
+    let totalAge = 0;
+    let medianAge = [];
+
+    let dtoOut = {
+        total: 0, 
+        averageAge: 0, 
+        minAge: 0, 
+        maxAge: 0, 
+        medianAge: 0,
+        ages: [],
+      };
+
+    for (var i = 0; i < dtoIn.classroom.length; i++) {
+
+    var age = new Date(dtoIn.classroom[i].birthdate);
+    if (age < minBirthDate) {
+        minBirthDate = age;
+        dtoOut.maxAge = getAge(age);
+    }
+
+    if (age > maxBirthDate) {
+        maxBirthDate = age;
+        dtoOut.minAge = getAge(age);
+    }
+
+    var currentAge = getAge(age);
+    totalAge += currentAge;
+
+    medianAge.push(currentAge);
+    dtoOut.ages.push(currentAge);
+}
+    dtoOut.medianAge = getMedian(medianAge);
+    dtoOut.averageAge = totalAge / dtoIn.classroom.length;
+    dtoOut.total = dtoIn.classroom.length
+    return dtoOut;
+}
+
+function validate(dtoIn){
+if (typeof(dtoIn.classroom) == "undefined"  
+    || dtoIn.classroom === null  
+    || dtoIn.classroom.length === 0)
+    {
+        alert("Please enter your data");
+    }
+}
+
+function getMedian(values) {
+    values.sort(function (a, b) {
+      return a - b;
+    });
+}
+
+function getAge(age){
+    return new Date().getFullYear() - age.getFullYear();
 }
 //@@viewOff:main
