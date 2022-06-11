4050c7fc04bb7adb49e3c49e6ae113b78b4a1e29
Novotnde <novotnde@hotmail.com>
2022-06-11 19:15:14 +0200

    added logic

--- students/task05/task.js
+++ students/task05/task.js
@@ -10,5 +10,49 @@
  * @return {object} output data
 **/
 function main(dtoIn={}) {
+
+    if (!validateInput(dtoIn)) {
+        console.log('validace neuspesna');
+        return;
+    }
+
+    let dtoOut = [
+        {
+          name: "...", // jméno
+          surname: "...", // přijmení
+          gender: "...", // pohlaví, hodnota male | female
+          age: 0 // věk
+        }
+      ];
+
+
+    let currentDate = new Date();
+    for (let i = 0; i < dtoIn. classroom.length; i++) {
+        let birthdate = new Date(dtoIn.classroom[i].birthdate);
+        if(birthdate.getMonth() == currentDate.getMonth())
+        {
+            dtoOut.push(dtoIn.classroom[i]);
+        }
+    }
+
+    return dtoOut;
 }
+
+function validateInput(dtoIn) {
+
+    if(dtoIn.classroom.length < 0){
+        throw "no data"
+    }
+    for (let i = 0; i < dtoIn.length; i++) {
+        if (dtoIn.classroom[i].gender.toLowerCase() != "male" && dtoIn.classroom[i].gender.toLowerCase() != "female") {
+            throw 'wrong gender.'
+        } else if (dtoIn.classroom[i].name == undefined || dtoIn.classroom[i].name == null ) {
+            throw "name undefined"
+        } else if (dtoIn.classroom[i].surname == undefined || dtoIn.classroom[i].surname == null ) {
+            throw "surname undefined"
+        }
+    }
+    return true;
+}
+
 //@@viewOff:main
