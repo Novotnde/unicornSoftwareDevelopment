63ec0723277c3c6b5770ee8a96eb9d92e52ad8a8
Novotnde <novotnde@hotmail.com>
2022-06-11 07:47:50 +0200

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+let fraction = 0.1 + 0.2;
+console.log(fraction);
+const x = 2n ** 53n;
+console.log(x);
+//Expected console log: 9007199254740992n
+console.log(42_000);
+
+let str = `This is a string.
+It has two lines.`;
+console.log(str)
+
+function sum (a,b){
+ return a + b;
+}
+
+log.console(sum(5,10)); 
