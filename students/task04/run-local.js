3bfffec4514f78b720fd71880acb7cf4610c812d
Novotnde <novotnde@hotmail.com>
2022-06-11 17:56:57 +0200

    added validation, logic

--- students/task04/run-local.js
+++ students/task04/run-local.js
@@ -4,7 +4,16 @@ const path = require("path");
 const dtoIn = {
   top: 0.5,
   classroom: [
-    {"gender":"male","birthdate":"1974-11-18T23:00:00.000Z","name":"Aleš","surname":"Bartoš"},{"gender":"male","birthdate":"1991-08-09T22:00:00.000Z","name":"Tomáš","surname":"Šimek"},{"gender":"female","birthdate":"1977-09-12T23:00:00.000Z","name":"Věra","surname":"Procházková"},{"gender":"male","birthdate":"1990-06-24T22:00:00.000Z","name":"Miloslav","surname":"Jelínek"},{"gender":"female","birthdate":"1978-01-22T23:00:00.000Z","name":"Anna","surname":"Křížová"},{"gender":"male","birthdate":"1987-01-09T23:00:00.000Z","name":"Roman","surname":"Růžička"},{"gender":"female","birthdate":"1999-05-19T22:00:00.000Z","name":"Veronika","surname":"Poláková"},{"gender":"female","birthdate":"1991-08-16T22:00:00.000Z","name":"Michaela","surname":"Růžičková"},{"gender":"female","birthdate":"1972-06-12T23:00:00.000Z","name":"Růžena","surname":"Fialová"},{"gender":"male","birthdate":"1973-04-22T23:00:00.000Z","name":"Štěpán","surname":"Doležal"}]
+    {"gender":"male","birthdate":"1974-11-18T23:00:00.000Z","name":"Al9999eš","surname":"Bartoš"},
+    {"gender":"male","birthdate":"1991-08-09T22:00:00.000Z","name":"Tomáš","surname":"Šimek"},
+    {"gender":"female","birthdate":"1977-09-12T23:00:00.000Z","name":"Věra","surname":"Procházková"},
+    {"gender":"male","birthdate":"1990-06-24T22:00:00.000Z","name":"Miloslav","surname":"Jelínek"},
+    {"gender":"female","birthdate":"1978-01-22T23:00:00.000Z","name":"Anna","surname":"Křížová"},
+    {"gender":"male","birthdate":"1987-01-09T23:00:00.000Z","name":"Roman","surname":"Růžička"},
+    {"gender":"female","birthdate":"1999-05-19T22:00:00.000Z","name":"Veronika","surname":"Poláková"},
+    {"gender":"female","birthdate":"1991-08-16T22:00:00.000Z","name":"Michaela","surname":"Růžičková"},
+    {"gender":"female","birthdate":"1972-06-12T23:00:00.000Z","name":"Růžena","surname":"Fialová"},
+    {"gender":"male","birthdate":"1973-04-22T23:00:00.000Z","name":"Štěpán","surname":"Doležal"}]
 };
 
 async function runFile(file) {
--- students/task04/task.js
+++ students/task04/task.js
@@ -10,5 +10,114 @@
  * @return {object} output data
 **/
 function main(dtoIn={}) {
+    if (!validate(dtoIn)) {
+        console.log('validace neuspesna');
+        return;
+    }
+   return GetData(dtoIn);
+
 }
+
+function validate(dtoIn) {
+
+    if(dtoIn.classroom.length < 0){
+        throw "no data"
+    }
+    for (var i = 0; i < dtoIn.length; i++) {
+        if (dtoIn.classroom[i].gender.toLowerCase() != "male" && dtoIn.classroom[i].gender.toLowerCase() != "female") {
+            throw 'wrong gender.'
+        } else if (dtoIn.classroom[i].name == undefined || dtoIn.classroom[i].name == null ) {
+            throw "name undefined"
+        } else if (dtoIn.classroom[i].surname == undefined || dtoIn.classroom[i].surname == null ) {
+            throw "surname undefined"
+        }
+    }
+    return true;
+}
+
+
+function GetData(dtoIn){
+
+    let dtoOut = {
+        names: {
+            all: {},
+            male: {},
+            female: {},
+        },
+        chartData: { 
+            all: [],
+            male: [],
+            female: [],
+        }
+    };
+    for (var i = 0; i < dtoIn.classroom.length; i++) {
+
+         CountAllNames(dtoOut, dtoIn, i);
+
+        if (dtoIn.classroom[i].gender.toLowerCase() == "female"){
+            CountFemaleNames(dtoOut, dtoIn, i);
+        }
+        if (dtoIn.classroom[i].gender.toLowerCase() == "male"){
+            CountMaleNames(dtoOut, dtoIn, i);
+        }
+    }
+
+       for (const property in dtoOut.names.all) {
+        dtoOut.chartData.all.push({
+            label: `${property}`,
+            value: `${dtoOut.names.all[property]}`
+        });
+      }
+
+      for (const property in dtoOut.names.male) {
+        dtoOut.chartData.male.push({
+            label: `${property}`,
+            value: `${dtoOut.names.male[property]}`
+        });
+      }
+      
+      for (const property in dtoOut.names.female) {
+        dtoOut.chartData.female.push({
+            label: `${property}`,
+            value: `${dtoOut.names.female[property]}`
+        });
+      }
+
+    return dtoOut;
+}
+
+function percentage(partialValue, totalValue) {
+    return (100 * partialValue) / totalValue;
+ } 
+
+function CountAllNames(dtoOut, dtoIn, i) {
+    if (dtoOut.names.all.hasOwnProperty(dtoIn.classroom[i].name)) {
+        dtoOut.names.all[dtoIn.classroom[i].name]++;
+      
+    }
+    else {
+        dtoOut.names.all[dtoIn.classroom[i].name] = 1;
+    }
+}
+
+function CountFemaleNames(dtoOut, dtoIn, i) {
+    if (dtoOut.names.female.hasOwnProperty(dtoIn.classroom[i].name)) {
+        dtoOut.names.female[dtoIn.classroom[i].name]++;
+      
+    }
+    else {
+        dtoOut.names.female[dtoIn.classroom[i].name] = 1;
+    }
+}
+
+function CountMaleNames(dtoOut, dtoIn, i) {
+    if (dtoOut.names.male.hasOwnProperty(dtoIn.classroom[i].name)) {
+        dtoOut.names.male[dtoIn.classroom[i].name]++;
+      
+    }
+    else {
+        dtoOut.names.male[dtoIn.classroom[i].name] = 1;
+    }
+}
+
 //@@viewOff:main
